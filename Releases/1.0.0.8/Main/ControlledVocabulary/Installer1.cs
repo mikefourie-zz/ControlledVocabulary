5e4bc841a439d65e102865e0863bde84d2e05870
mikeFourie_cp <SND\mikeFourie_cp>
2010-09-23 10:42:17 +0100

    Branched from $/ControlledVocabulary/Main

---
+++ Releases/1.0.0.8/Main/ControlledVocabulary/Installer1.cs
@@ -0,0 +1,43 @@
+﻿//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// <copyright file="Installer1.cs">(c) Controlled Vocabulary on CodePlex, 2010. All other rights reserved.</copyright>
+//--------------------------------------------------------------------------------------------------------------------------------
+namespace ControlledVocabulary
+{
+    using System.Collections;
+    using System.ComponentModel;
+    using System.Diagnostics;
+
+    /// <summary>
+    /// Installer Class
+    /// </summary>
+    [RunInstaller(true)]
+    public partial class Installer1 : System.Configuration.Install.Installer
+    {
+        public Installer1()
+        {
+            this.InitializeComponent();
+            if (!EventLog.SourceExists("ControlledVocabulary"))
+            {
+                EventLog.CreateEventSource("ControlledVocabulary", "Application");
+            }
+        }
+
+        protected override void OnBeforeInstall(IDictionary savedState)
+        {
+            try
+            {
+                Process[] processes = Process.GetProcessesByName("OUTLOOK");
+                foreach (Process process in processes)
+                {
+                    process.Kill();
+                }
+            }
+            catch
+            {
+                // Do Nothing
+            }
+
+            base.OnBeforeInstall(savedState);
+        }
+    }
+}

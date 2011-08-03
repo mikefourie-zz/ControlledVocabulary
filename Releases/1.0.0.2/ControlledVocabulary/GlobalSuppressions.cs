8d8593d9a74399d1c35d7a09328c50ea3f4c4271
mikeFourie_cp <SND\mikeFourie_cp>
2011-08-03 22:52:15 +0100

    Release cleanup

--- Releases/1.0.0.2/ControlledVocabulary/GlobalSuppressions.cs
+++
@@ -1,8 +0,0 @@
-﻿//--------------------------------------------------------------------------------------------------------------------------------
-// <copyright file="GlobalSuppressions.cs">(c) Controlled Vocabulary on Codeplex, 2010. All other rights reserved.</copyright>
-//--------------------------------------------------------------------------------------------------------------------------------
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA2210:AssembliesShouldHaveValidStrongNames")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate", Scope = "member", Target = "ControlledVocabulary.StaticHelper.#GetButtons()")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate", Scope = "member", Target = "ControlledVocabulary.StaticHelper.#GetInstallationPath()")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Scope = "namespace", Target = "ControlledVocabulary")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1055:UriReturnValuesShouldNotBeStrings", Scope = "member", Target = "ControlledVocabulary.StaticHelper.#GetGuidanceUrl(System.String)")]
--- Releases/1.1.0.0/Main/WindowsClient/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿//--------------------------------------------------------------------------------------------------------------------------------
-// <copyright file="AssemblyInfo.cs">(c) Controlled Vocabulary on CodePlex, 2010. All other rights reserved.</copyright>
-//--------------------------------------------------------------------------------------------------------------------------------
-using System.Reflection;
-using System.Windows;
-
-[assembly: AssemblyTitle("Controlled Vocabulary Windows Client")]
-[assembly: AssemblyDescription("Controlled Vocabulary Windows Client")]
-[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]

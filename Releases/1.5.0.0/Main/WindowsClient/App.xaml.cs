80094ce8e0e405980bf1c30b7cfb04e13aa21ac9
mikeFourie_cp <SND\mikeFourie_cp>
2011-08-03 22:51:02 +0100

    Branched from $/ControlledVocabulary/Main

---
+++ Releases/1.5.0.0/Main/WindowsClient/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿//--------------------------------------------------------------------------------------------------------------------------------
+// <copyright file="App.xaml.cs">(c) Controlled Vocabulary on CodePlex, 2010. All other rights reserved.</copyright>
+//--------------------------------------------------------------------------------------------------------------------------------
+namespace ControlledVocabulary
+{
+    using System;
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private static void MyHandler(object sender, UnhandledExceptionEventArgs args)
+        {
+            Exception e = (Exception)args.ExceptionObject;
+            Error errorWindow = new Error(e);
+            errorWindow.ShowDialog();
+            Environment.Exit(-1);
+        }
+
+        private void App_StartUp(object sender, StartupEventArgs e)
+        {
+            AppDomain currentDomain = AppDomain.CurrentDomain;
+            currentDomain.UnhandledException += MyHandler;
+
+            if (e.Args.Length > 0)
+            {
+                Manager managerWindow = new Manager(e.Args[0]);
+                managerWindow.ShowDialog();
+            }
+        }
+    }
+}

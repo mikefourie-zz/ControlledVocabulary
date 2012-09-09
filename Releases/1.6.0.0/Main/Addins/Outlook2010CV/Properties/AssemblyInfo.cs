6799a1cfed8a356330b041d7b80321c73903c656
mikeFourie_cp <SND\mikeFourie_cp>
2012-09-09 14:26:39 +0100

    Branched from $/ControlledVocabulary/Main

---
+++ Releases/1.6.0.0/Main/Addins/Outlook2010CV/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿//--------------------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs">(c) Controlled Vocabulary on CodePlex, 2010. All other rights reserved.</copyright>
+//--------------------------------------------------------------------------------------------------------------------------------
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Controlled Vocabulary")]
+[assembly: AssemblyDescription("Controlled Vocabulary")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1e456f7d-8733-4216-bcb4-2b2c7637cdf3")]
